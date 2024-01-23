2523677ad4247f386f4ad4ee2cc696e303eeaf02
Michael Hazen <michael@hazen.zone>
2024-01-23 13:34:57 -0800

    created a test that actually did something. added a "clean up" test to go at the end.

--- cypress/e2e/trelloTestspec.cy.js
+++ cypress/e2e/trelloTestspec.cy.js
@@ -6,32 +6,59 @@ describe('My first Cypress Tests',() => {
         Cypress.config('baseUrl','http://localhost:3000');
     })
     //validate initial state
-    it('homepage', () => {
+    //Im not really good at getting cypress to go deep in the dom
+    //So I will add more validations as I learn about them
+    it('homepage initial state validation', () => {
         cy
         .visit("/");
 
         cy.get('.Nav')
-        .should("be.visible")
+            .should("be.visible")
         
         cy.get('.Nav_logo')
-        .should("be.visible")
+            .should("be.visible")
 
         cy.get('[data-cy="login-menu"]')
-        .should("be.visible")
-        .should("have.text", "\n        Log in\n      ")
+            .should("be.visible")
+            .should("have.text", "\n        Log in\n      ")
 
         cy.get(".background_title")
-        .should("be.visible")
-        .contains("My Boards")//probably not a great validation. But I havent figured out how to get a node data = text to work yet. I'll probably kick myslef when I figure it out
+            .should("be.visible")
+            .contains("My Boards")//probably not a great validation. But I havent figured out how to get a node data = text to work yet. I'll probably kick myslef when I figure it out
 
         cy.get('[data-cy="create-board"]')
-        .should("be.visible")
+            .should("be.visible")
         
         cy.get('[data-cy="create-board"]')
-        .should("be.visible")
-        
-
+            .should("be.visible")
         })
+    it('Initial homepage but now we do stuff', () => {
+        cy.visit("/")
+        /* ==== Generated with Cypress Studio ==== */
+        cy.get('[data-cy="create-board"]')
+            .click()
+        cy.get('[data-cy="new-board-input"]')
+            .type('I create my first board{enter}');
+        cy.get('[data-cy="add-list"]')
+            .click();
+        /* ==== End Cypress Studio ==== */
+    })
+    it('Clear the board - test cleanup', () => {
+        cy.visit("/")
+        /*
+        this is an example of me overthinking it. 
+        I needed to bring up a hidden menu that let me clear the data created by tests
+        I spent a good hour trying to get F2 to play and reveal the menu. 
+        but when I finally poked through the DOM, I found that there was
+        just a hidden flag. so Force: True was all I really needed. 
+        s
+        cy.get('body').trigger('keydown', { keyCode: 113, which: 113 });
+        */
+        cy.get('#tools > :nth-child(1)')
+            .click({force: true}) 
+
+
+    })
 
 
 })
